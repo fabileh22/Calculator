b7d8c986f653e803bef8481e7a88089d0a2d4953
fabileh22 <lehmann.fabio@gmx.de>
2023-05-04 12:23:27 +1000

    Initial Commit
    Initial Commit - Configured template, Back end setup

---
+++ Calculator/Controllers/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CalculatorController : ControllerBase
+{
+    [HttpGet]
+    public string Get(double val1, char operation, double val2)
+    {
+        switch(operation)
+        {
+            case '+':
+                return (val1 + val2).ToString();
+            case '-':
+                return (val1 - val2).ToString();
+            case '*':
+                return (val1 * val2).ToString();
+            case '/':
+                return (val1 / val2).ToString();
+            default:
+                return "Please enter a valid operator (+, -, * or /)";
+
+        }        
+    }
+}
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿//FL 04.05.23
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Sets up a service for the controllers
+builder.Services.AddControllers();
+
+//Adds an endpoint of communication to assure the SwaggerUI
+builder.Services.AddEndpointsApiExplorer();
+
+//Adds the necessary Swagger services for the application to run
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//Assures that we are running the app from the Development Environment
+//Different environments can be added later
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Forwards the URL to multiple locations if inital location is unavailable
+app.UseHttpsRedirection();
+
+//Authenticates User to access "secure" recources
+app.UseAuthorization();
+
+//Maps data to the web object in order to run the controllers
+app.MapControllers();
+
+app.Run();
+
