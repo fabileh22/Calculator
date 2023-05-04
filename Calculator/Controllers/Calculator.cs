941881e717b8b7c2d0714d70e3694569896a3fa5
fabileh22 <lehmann.fabio@gmx.de>
2023-05-04 12:46:34 +1000

    Update Calculator.cs
    Comments added

--- Calculator/Controllers/Calculator.cs
+++ Calculator/Controllers/Calculator.cs
@@ -2,6 +2,9 @@
 
 namespace Calculator.Controllers;
 
+//FL 04.05.23
+
+//Controller to execute the GET-request
 [ApiController]
 [Route("[controller]")]
 public class CalculatorController : ControllerBase
@@ -9,6 +12,8 @@ public class CalculatorController : ControllerBase
     [HttpGet]
     public string Get(double val1, char operation, double val2)
     {
+        //Depending on which operator is selected, a result is created
+        //The Operators can only be +, -, * and / to generate a result
         switch(operation)
         {
             case '+':
@@ -21,7 +26,6 @@ public class CalculatorController : ControllerBase
                 return (val1 / val2).ToString();
             default:
                 return "Please enter a valid operator (+, -, * or /)";
-
         }        
     }
 }
